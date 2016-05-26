484df5ca473def68b28204ec5515425950e03422
johnsonmoon <841846248@qq.com>
2016-05-26 14:11:56 +0800

    First commit.

---
+++ src/com/xuyihao/Test/JohnsonTestMain.java
@@ -0,0 +1,14 @@
+package com.xuyihao.Test;
+
+/**
+ * created by xuyihao on 2016/5/26
+ * @author Johnson
+ * @description java抓包工具测试入口
+ * */
+public class JohnsonTestMain {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
