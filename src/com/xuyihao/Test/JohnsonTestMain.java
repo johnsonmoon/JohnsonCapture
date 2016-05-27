4742cc54279806e9a66e15d045940a8f092bbd72
johnson_moon <841846248@qq.com>
2016-05-27 20:17:45 +0800

    完成ubuntu下配置依赖以及windows下配置依赖,由于ubuntu下需要root运行,转向windows下开发

--- src/com/xuyihao/Test/JohnsonTestMain.java
+++ src/com/xuyihao/Test/JohnsonTestMain.java
@@ -1,14 +1,37 @@
 package com.xuyihao.Test;
 
+import com.xuyihao.tools.JohnsonPacketReceiver;
+
+import jpcap.JpcapCaptor;
+import jpcap.NetworkInterface;
+
 /**
  * created by xuyihao on 2016/5/26
  * @author Johnson
  * @description java抓包工具测试入口
  * */
-public class JohnsonTestMain {
-
-	public static void main(String[] args) {
-		
+public class JohnsonTestMain{
+	public static void main(String[] args){
+		try{
+			final NetworkInterface[] devices = JpcapCaptor.getDeviceList();
+			for(int i = 0; i < devices.length; i ++){
+				NetworkInterface nif = devices[i];
+				JpcapCaptor jpcap = JpcapCaptor.openDevice(nif, 2000, true, 20);
+				startCapThread(jpcap);
+				System.out.println("开始抓取第"+i+"个卡口上的数据");
+			}
+		}catch(Exception e){
+			e.printStackTrace();
+			System.out.println("启动失败!");
+		}
 	}
-
+	
+	public static void startCapThread(final JpcapCaptor jpcap){
+		new Thread(){
+			@Override
+			public void run(){
+				jpcap.loopPacket(-1, new JohnsonPacketReceiver());
+			}
+		}.start();
+	}
 }
---
+++ src/com/xuyihao/tools/JohnsonPacketReceiver.java
@@ -0,0 +1,52 @@
+package com.xuyihao.tools;
+
+import jpcap.PacketReceiver;
+import jpcap.packet.ARPPacket;
+import jpcap.packet.DatalinkPacket;
+import jpcap.packet.EthernetPacket;
+import jpcap.packet.ICMPPacket;
+import jpcap.packet.Packet;
+import jpcap.packet.TCPPacket;
+import jpcap.packet.UDPPacket;
+
+/**
+ * @author johnson
+ * created by xuyihao on 2016/5/27
+ * @description a listener for listening package receiving
+ * */
+public class JohnsonPacketReceiver implements PacketReceiver{
+
+	@Override
+	public void receivePacket(Packet p) {
+		//if it is a TCP package
+		if(p instanceof jpcap.packet.TCPPacket){
+			TCPPacket packet = (TCPPacket)p;
+			String s = "UDP Packet : dst_ip "+packet.dst_ip+":"+packet.dst_port+" | src_ip "+packet.src_ip+":"+packet.src_port+" | len: "+packet.len+"";
+			System.out.println(s);
+		}else if(p instanceof jpcap.packet.UDPPacket){
+			UDPPacket packet = (UDPPacket)p;
+			String s = "UDP Packet : dst_ip "+packet.dst_ip+":"+packet.dst_port+" | src_ip "+packet.src_ip+":"+packet.src_port+" | len: "+packet.len+"";
+			System.out.println(s);
+		}else if(p instanceof jpcap.packet.ICMPPacket){
+			ICMPPacket packet = (ICMPPacket)p;
+			String router_ip = "";
+			for(int i = 0; i < packet.router_ip.length; i++){
+				router_ip += " " + packet.router_ip[i].getHostAddress();
+			}
+			String s = "@ @ @ ICMPPacket: | router_ip: "+router_ip+" | redir_ip: "+packet.redir_ip+" | mtu: "+packet.mtu+" | length: "+packet.length+"";
+			System.out.println(s);
+		}else if(p instanceof jpcap.packet.ARPPacket){
+			ARPPacket packet = (ARPPacket)p;
+			Object saa = packet.getSenderHardwareAddress();
+			Object taa = packet.getTargetHardwareAddress();
+			String s = "* * * ARPPacket: | SenderHardwareAddress "+saa+" | TargetHardwareAddress "+taa+" | len: "+packet.len+"";
+			System.out.println(s);
+		}
+		DatalinkPacket datalink = p.datalink;
+		if(datalink instanceof jpcap.packet.EthernetPacket){
+			EthernetPacket packet = (EthernetPacket)datalink;
+			String s = " datalink layer packet: |DestinationAddress: "+packet.getDestinationAddress()+" | SourceAddress: "+packet.getSourceAddress()+"";
+			System.out.println(s);
+		}
+	}
+}
