f0092ccd37781e8a81c9f8789d6f0f0f7190ce61
johnsonmoon <841846248@qq.com>
2016-06-04 16:55:33 +0800

    修改打印显示的几个数据包参数

--- src/com/xuyihao/tools/JohnsonPacketReceiver.java
+++ src/com/xuyihao/tools/JohnsonPacketReceiver.java
@@ -51,34 +51,16 @@ public class JohnsonPacketReceiver implements PacketReceiver{
 				System.out.println("设备描述: "+this.deviceDescription+"");
 			}
 			System.out.println("版本号(version):           "+packet.version);
-			System.out.println("数据包长度(len):            "+packet.len);
-			System.out.println("标志位(r_flag):            "+packet.r_flag);
+			System.out.println("数据包长度(length):         "+packet.length);
 			System.out.println("标志位(more_frag):         "+packet.more_frag);
+			System.out.println("标志位(dont_frag):         "+packet.dont_frag);
 			System.out.println("标志位(rsv_frag):          "+packet.rsv_frag);
+			System.out.println("TTL(hop_limit):           "+packet.hop_limit);
 			System.out.println("片偏移(Fragment offset):   "+packet.offset);
 			System.out.println("协议(protocol):            "+packet.protocol);
+			System.out.println("优先级(priority):          "+packet.priority);
 			System.out.println("原地址IP(src_ip):          "+packet.src_ip);
 			System.out.println("目的地址IP(dst_ip):         "+packet.dst_ip);
-			/*
-			System.out.println("caplen:   "+packet.caplen);
-			System.out.println("d_flag:   "+packet.d_flag);
-			System.out.println("dont_frag:   "+packet.dont_frag);
-			System.out.println("flow_label:   "+packet.flow_label);
-			System.out.println("ident:   "+packet.ident);
-			System.out.println("more_frag:   "+packet.more_frag);
-			System.out.println("rsv_frag:   "+packet.rsv_frag);
-			System.out.println("sec:   "+packet.sec);
-			System.out.println("t_flag:   "+packet.t_flag);
-			System.out.println("usec:   "+packet.usec);
-			System.out.println("toString:   "+packet.toString());
-			System.out.println("data:   "+packet.data);
-			System.out.println("header:   "+packet.header);
-			System.out.println("hop_limit:   "+packet.hop_limit);
-			System.out.println("length:   "+packet.length);
-			System.out.println("option:   "+packet.option);
-			System.out.println("priority:   "+packet.priority);
-			System.out.println("rsv_tos:   "+packet.rsv_tos);
-			*/
 		}
 		if(this.threadWaitTime > 0){
 			try {
