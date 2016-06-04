ebb02d8862688ba8568f3e0de45fcf2039463990
johnsonmoon <841846248@qq.com>
2016-06-04 16:01:49 +0800

    移除除IP包以外的抓包，保留抓取的IP包等命令行打印的参数信息，完善主方法运行逻辑

--- src/com/xuyihao/Test/JohnsonTestMain.java
+++ src/com/xuyihao/Test/JohnsonTestMain.java
@@ -2,6 +2,7 @@ package com.xuyihao.Test;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 import com.xuyihao.tools.JohnsonPacketReceiver;
 
@@ -18,29 +19,62 @@ public class JohnsonTestMain{
 		try{
 			final NetworkInterface[] devices = JpcapCaptor.getDeviceList();
 			for(int j = 0; j < devices.length; j++){
-				System.out.println("扫描到以下这些网口:  ");
+				//System.out.println("扫描到以下这些网口:  ");
+				System.out.println("网口:  ");
 				System.out.println("No."+j+":  "+devices[j].name+" | "+devices[j].description+"");
 			}
-			System.out.println("请选择需要监听的接口:");
+			
+			ArrayList<JpcapCaptor> captors = new ArrayList<>();
+			
+			System.out.println("请选择需要监听的接口(格式:1&2&3&4):");
 			BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 			String in = reader.readLine();
-			int selected = Integer.parseInt(in);
-			NetworkInterface nif = devices[selected];
-			JpcapCaptor jpcap = JpcapCaptor.openDevice(nif, 2000, true, 20);
-			startCapThread(jpcap);
-			System.out.println("开始抓取第"+selected+"个卡口上的数据");
-			
+			char[] ins = in.toCharArray();
+			System.out.println("请输入每条监听线程的线程等待时间(s):");
+			int timeIn = Integer.parseInt(reader.readLine());
+			for(int i = 0; i < ins.length; i++){
+				if(ins[i] != '&'){
+					int deviceNum = (int)ins[i] - 48;
+					NetworkInterface nif = devices[deviceNum];
+					try{
+						JpcapCaptor jpcap = JpcapCaptor.openDevice(nif, 2000, true, 20);
+						captors.add(jpcap);
+						startCapThread(jpcap, timeIn, deviceNum, devices[deviceNum].name, devices[deviceNum].description);
+					}catch(Exception e){
+						//do nothing
+					}
+					System.out.println("开启第"+ins[i]+"个设备监听");
+				}
+			}
+			String commandIn = "";
+			while(!commandIn.equals("exit")){
+				System.out.println("停止监听输入  exit ");
+				commandIn = reader.readLine();
+			}
+			for(int k = 0; k < captors.size(); k++){
+				captors.get(k).breakLoop();
+			}
+			System.out.println("监听结束! 程序结束!");
 		}catch(Exception e){
 			//e.printStackTrace();
 			System.out.println("启动失败!");
 		}
 	}
 	
-	public static void startCapThread(final JpcapCaptor jpcap){
+	public static void startCapThread(final JpcapCaptor jpcap, final int waitTime){
 		new Thread(){
 			@Override
 			public void run(){
-				jpcap.loopPacket(-1, new JohnsonPacketReceiver());
+				jpcap.loopPacket(-1, new JohnsonPacketReceiver(waitTime));
+			}
+		}.start();
+	}
+	
+	public static void startCapThread(final JpcapCaptor jpcap, final int waitTime, final int deviceNumber, final String deviceName, final String deviceDescription){
+		new Thread(){
+			@Override
+			public void run(){
+				jpcap.loopPacket(-1, new JohnsonPacketReceiver(waitTime, deviceNumber, deviceName, deviceDescription));
 			}
 		}.start();
 	}
--- src/com/xuyihao/tools/JohnsonPacketReceiver.java
+++ src/com/xuyihao/tools/JohnsonPacketReceiver.java
@@ -1,13 +1,8 @@
 package com.xuyihao.tools;
 
 import jpcap.PacketReceiver;
-import jpcap.packet.ARPPacket;
-import jpcap.packet.DatalinkPacket;
-import jpcap.packet.EthernetPacket;
-import jpcap.packet.ICMPPacket;
+import jpcap.packet.IPPacket;
 import jpcap.packet.Packet;
-import jpcap.packet.TCPPacket;
-import jpcap.packet.UDPPacket;
 
 /**
  * @author johnson
@@ -16,36 +11,81 @@ import jpcap.packet.UDPPacket;
  * */
 public class JohnsonPacketReceiver implements PacketReceiver{
 
+	/**
+	 * fields
+	 * */
+	//for controlling
+	private int threadWaitTime = 0;
+	private boolean hasDeviceInfomation = false;
+	private int deviceNumber = 0;
+	private String deviceName = "";
+	private String deviceDescription = "";
+	
+	/**
+	 * constructor
+	 * */
+	public JohnsonPacketReceiver(int waitTime) {
+		this.threadWaitTime = waitTime;
+		this.hasDeviceInfomation = false;
+	}
+	
+	/**
+	 * constructor2
+	 * */
+	public JohnsonPacketReceiver(int waitTime, int number, String name, String description){
+		this.threadWaitTime = waitTime;
+		this.hasDeviceInfomation = true;
+		this.deviceNumber = number;
+		this.deviceName = name;
+		this.deviceDescription = description;
+	}
+	
 	@Override
 	public void receivePacket(Packet p) {
-		if(p instanceof jpcap.packet.TCPPacket){
-			TCPPacket packet = (TCPPacket)p;
-			String s = "TCP Packet : dst_ip "+packet.dst_ip+":"+packet.dst_port+" | src_ip "+packet.src_ip+":"+packet.src_port+" | len: "+packet.len+"";
-			System.out.println(s);
-		}else if(p instanceof jpcap.packet.UDPPacket){
-			UDPPacket packet = (UDPPacket)p;
-			String s = "UDP Packet : dst_ip "+packet.dst_ip+":"+packet.dst_port+" | src_ip "+packet.src_ip+":"+packet.src_port+" | len: "+packet.len+"";
-			System.out.println(s);
-		}else if(p instanceof jpcap.packet.ICMPPacket){
-			ICMPPacket packet = (ICMPPacket)p;
-			String router_ip = "";
-			for(int i = 0; i < packet.router_ip.length; i++){
-				router_ip += " " + packet.router_ip[i].getHostAddress();
+		
+		if(p instanceof jpcap.packet.IPPacket){
+			IPPacket packet = (IPPacket)p;
+			System.out.println("\n\n\n");
+			if(this.hasDeviceInfomation){
+				System.out.println("设备: "+this.deviceNumber+"  ||  设备名: "+this.deviceName+"");
+				System.out.println("设备描述: "+this.deviceDescription+"");
 			}
-			String s = "@ @ @ ICMPPacket: | router_ip: "+router_ip+" | redir_ip: "+packet.redir_ip+" | mtu: "+packet.mtu+" | length: "+packet.length+"";
-			System.out.println(s);
-		}else if(p instanceof jpcap.packet.ARPPacket){
-			ARPPacket packet = (ARPPacket)p;
-			Object saa = packet.getSenderHardwareAddress();
-			Object taa = packet.getTargetHardwareAddress();
-			String s = "* * * ARPPacket: | SenderHardwareAddress "+saa+" | TargetHardwareAddress "+taa+" | len: "+packet.len+"";
-			System.out.println(s);
+			System.out.println("版本号(version):           "+packet.version);
+			System.out.println("数据包长度(len):            "+packet.len);
+			System.out.println("标志位(r_flag):            "+packet.r_flag);
+			System.out.println("标志位(more_frag):         "+packet.more_frag);
+			System.out.println("标志位(rsv_frag):          "+packet.rsv_frag);
+			System.out.println("片偏移(Fragment offset):   "+packet.offset);
+			System.out.println("协议(protocol):            "+packet.protocol);
+			System.out.println("原地址IP(src_ip):          "+packet.src_ip);
+			System.out.println("目的地址IP(dst_ip):         "+packet.dst_ip);
+			/*
+			System.out.println("caplen:   "+packet.caplen);
+			System.out.println("d_flag:   "+packet.d_flag);
+			System.out.println("dont_frag:   "+packet.dont_frag);
+			System.out.println("flow_label:   "+packet.flow_label);
+			System.out.println("ident:   "+packet.ident);
+			System.out.println("more_frag:   "+packet.more_frag);
+			System.out.println("rsv_frag:   "+packet.rsv_frag);
+			System.out.println("sec:   "+packet.sec);
+			System.out.println("t_flag:   "+packet.t_flag);
+			System.out.println("usec:   "+packet.usec);
+			System.out.println("toString:   "+packet.toString());
+			System.out.println("data:   "+packet.data);
+			System.out.println("header:   "+packet.header);
+			System.out.println("hop_limit:   "+packet.hop_limit);
+			System.out.println("length:   "+packet.length);
+			System.out.println("option:   "+packet.option);
+			System.out.println("priority:   "+packet.priority);
+			System.out.println("rsv_tos:   "+packet.rsv_tos);
+			*/
 		}
-		DatalinkPacket datalink = p.datalink;
-		if(datalink instanceof jpcap.packet.EthernetPacket){
-			EthernetPacket packet = (EthernetPacket)datalink;
-			String s = " datalink layer packet: |DestinationAddress: "+packet.getDestinationAddress()+" | SourceAddress: "+packet.getSourceAddress()+"";
-			System.out.println(s);
+		if(this.threadWaitTime > 0){
+			try {
+				Thread.sleep(this.threadWaitTime * 1000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
 		}
 	}
 }
