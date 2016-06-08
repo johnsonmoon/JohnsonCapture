044db8ba6b050e1f41ff6599868f361d5acb844d
johnsonmoon <841846248@qq.com>
2016-06-08 17:48:29 +0800

    添加中文注释，便于开发者理解

--- src/com/xuyihao/Test/JohnsonTestMain.java
+++ src/com/xuyihao/Test/JohnsonTestMain.java
@@ -12,47 +12,47 @@ import jpcap.NetworkInterface;
 /**
  * created by xuyihao on 2016/5/26
  * @author Johnson
- * @description java抓包工具测试入口
+ * @description java抓包工具测试主入口
+ * @description 主方法首先通过JpcapCaptor获取计算机上面的所有网络接口的列表
+ * @description 然后将其打印出来，最后根据用户输入的需要监听的接口，对相应的
+ * @description 接口设置相应的监听器，并为每个监听开启一个子线程，进行监听
  * */
 public class JohnsonTestMain{
 	public static void main(String[] args){
 		try{
-			final NetworkInterface[] devices = JpcapCaptor.getDeviceList();
-			for(int j = 0; j < devices.length; j++){
-				//System.out.println("扫描到以下这些网口:  ");
+			final NetworkInterface[] devices = JpcapCaptor.getDeviceList();//通过JpcapCaptor获取网络接口设备列表
+			for(int j = 0; j < devices.length; j++){//将网络设备打印到控制台
 				System.out.println("网口:  ");
 				System.out.println("No."+j+":  "+devices[j].name+" | "+devices[j].description+"");
 			}
 			
-			ArrayList<JpcapCaptor> captors = new ArrayList<>();
+			ArrayList<JpcapCaptor> captors = new ArrayList<>();//定义一个JpcapCaptor的列表
 			
 			System.out.println("请选择需要监听的接口(格式:1&2&3&4):");
-			BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-			String in = reader.readLine();
+			BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));//设置输入流
+			String in = reader.readLine();//用户输入需要监听的网络接口编号
 			char[] ins = in.toCharArray();
 			System.out.println("请输入每条监听线程的线程等待时间(s):");
-			int timeIn = Integer.parseInt(reader.readLine());
-			for(int i = 0; i < ins.length; i++){
-				if(ins[i] != '&'){
-					int deviceNum = (int)ins[i] - 48;
-					NetworkInterface nif = devices[deviceNum];
-					try{
-						JpcapCaptor jpcap = JpcapCaptor.openDevice(nif, 2000, true, 20);
-						captors.add(jpcap);
-						startCapThread(jpcap, timeIn, deviceNum, devices[deviceNum].name, devices[deviceNum].description);
-					}catch(Exception e){
-						//do nothing
-						e.printStackTrace();
-					}
-					System.out.println("开启第"+ins[i]+"个设备监听");
+			int timeIn = Integer.parseInt(reader.readLine());//用户输入每条监听线程的线程等待（阻塞）时间
+			for(int i = 0; i < ins.length; i++){//下面根据用户的输入，对相应的接口设置监听并且开启监听线程
+				int deviceNum = (int)ins[i] - 48;
+				NetworkInterface nif = devices[deviceNum];
+				try{
+					JpcapCaptor jpcap = JpcapCaptor.openDevice(nif, 2000, true, 20);
+					captors.add(jpcap);
+					startCapThread(jpcap, timeIn, deviceNum, devices[deviceNum].name, devices[deviceNum].description);
+				}catch(Exception e){
+					//do nothing
+					e.printStackTrace();
 				}
+				System.out.println("开启第"+ins[i]+"个设备监听");
 			}
-			String commandIn = "";
-			while(!commandIn.equals("exit")){
+			String commandIn = "";//监听开启后，定义一个控制变量用来控制子线程是否关闭（子线程停止监听）
+			while(!commandIn.equals("exit")){//如果输入了exit则说明用户希望停止所有的监听线程，如果输入不为exit则继续循环
 				System.out.println("停止监听输入  exit ");
-				commandIn = reader.readLine();
+				commandIn = reader.readLine();//输入指令
 			}
-			for(int k = 0; k < captors.size(); k++){
+			for(int k = 0; k < captors.size(); k++){//exit指令获取后，对接口列表的接口作停止监听操作
 				captors.get(k).breakLoop();
 			}
 			System.out.println("监听结束! 程序结束!");
@@ -62,6 +62,11 @@ public class JohnsonTestMain{
 		}
 	}
 	
+	/**
+	 * @author johnson
+	 * @method startCapThread
+	 * @description 开启线程的方法
+	 * */
 	public static void startCapThread(final JpcapCaptor jpcap, final int waitTime){
 		new Thread(){
 			@Override
@@ -71,6 +76,11 @@ public class JohnsonTestMain{
 		}.start();
 	}
 	
+	/**
+	 * @author johnson
+	 * @method startCapThread
+	 * @description 开启线程的方法 
+	 * */
 	public static void startCapThread(final JpcapCaptor jpcap, final int waitTime, final int deviceNumber, final String deviceName, final String deviceDescription){
 		new Thread(){
 			@Override
--- src/com/xuyihao/tools/JohnsonPacketReceiver.java
+++ src/com/xuyihao/tools/JohnsonPacketReceiver.java
@@ -8,6 +8,7 @@ import jpcap.packet.Packet;
  * @author johnson
  * created by xuyihao on 2016/5/27
  * @description a listener for listening package receiving
+ * @description 继承自PacketReceiver 的工具类，用来监听并抓取特定网络接口的数据报发送 
  * */
 public class JohnsonPacketReceiver implements PacketReceiver{
 
@@ -15,38 +16,53 @@ public class JohnsonPacketReceiver implements PacketReceiver{
 	 * fields
 	 * */
 	//for controlling
-	private int threadWaitTime = 0;
-	private boolean hasDeviceInfomation = false;
-	private int deviceNumber = 0;
-	private String deviceName = "";
-	private String deviceDescription = "";
+	private int threadWaitTime = 0;//线程等待时间，即抓包监听线程手动阻塞的时间，便于观察控制台下的抓包情况，在等待时间之内，线程是无法获取到监听到的数据报的
+	private boolean hasDeviceInformation = false;//判断是否有此网络接口信息的一个布尔值，用构造器进行初始化
+	private int deviceNumber = 0;//网络接口信息，接口号（程序定），由构造器初始化
+	private String deviceName = "";//网络接口信息，接口名（由Jpcap通过操作系统获得），由构造器初始化
+	private String deviceDescription = "";//网络接口的描述信息，（由Jpcap通过操作系统获取），由构造器初始化
 	
 	/**
 	 * constructor
+	 * @description 默认构造器
+	 * @param waitTime 单个抓包线程需要等待（手动阻塞）的时间
 	 * */
 	public JohnsonPacketReceiver(int waitTime) {
 		this.threadWaitTime = waitTime;
-		this.hasDeviceInfomation = false;
+		this.hasDeviceInformation = false;
 	}
 	
 	/**
 	 * constructor2
+	 * @author johnson
+	 * @description 构造器2
+	 * @param waitTime 单个抓包线程需要等待（手动阻塞）的时间
+	 * @param number 初始化传入的设备编号
+	 * @param name 初始化传入的设备名
+	 * @param description 初始化传入的设备描述
 	 * */
 	public JohnsonPacketReceiver(int waitTime, int number, String name, String description){
 		this.threadWaitTime = waitTime;
-		this.hasDeviceInfomation = true;
+		this.hasDeviceInformation = true;
 		this.deviceNumber = number;
 		this.deviceName = name;
 		this.deviceDescription = description;
 	}
 	
+	/**
+	 * @author johnson
+	 * @method receivePacket
+	 * @description 抓包之后的核心方法，主要功能都在其中实现，重写自几类PacketReceiver
+	 * @description 抓包之后，系统自动执行该方法，需要执行的操作就在方法体内完成
+	 * */
 	@Override
 	public void receivePacket(Packet p) {
 		
-		if(p instanceof jpcap.packet.IPPacket){
+		if(p instanceof jpcap.packet.IPPacket){//判断抓取的数据报p是否为IP数据报
 			IPPacket packet = (IPPacket)p;
+			//下面一系列操作讲IP数据报的各项参数打印到控制台
 			System.out.println("\n\n\n");
-			if(this.hasDeviceInfomation){
+			if(this.hasDeviceInformation){
 				System.out.println("设备: "+this.deviceNumber+"  ||  设备名: "+this.deviceName+"");
 				System.out.println("设备描述: "+this.deviceDescription+"");
 			}
@@ -62,7 +78,7 @@ public class JohnsonPacketReceiver implements PacketReceiver{
 			System.out.println("原地址IP(src_ip):          "+packet.src_ip);
 			System.out.println("目的地址IP(dst_ip):         "+packet.dst_ip);
 		}
-		if(this.threadWaitTime > 0){
+		if(this.threadWaitTime > 0){//如果设置的线程等待时间大于零，即需要等待，则手动设置线程睡眠设置的时间
 			try {
 				Thread.sleep(this.threadWaitTime * 1000);
 			} catch (InterruptedException e) {
